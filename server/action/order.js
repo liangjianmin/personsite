e4a3755912095138759776153ae096fcf4c277a5
liangjianmin <jianmin.liang@dolabank.com>
2017-05-27 10:58:16 +0800

    判断库存为0的时候删除该商品

--- server/action/order.js
+++ server/action/order.js
@@ -1,17 +1,17 @@
 var shop = require("../models/shop.js");
-var comment=require("../models/comment.js");
-var order=require("../models/order.js");
+var comment = require("../models/comment.js");
+var order = require("../models/order.js");
 var moment = require('moment'); //时间
 var multiparty = require('multiparty'); //文件操作模块
 var util = require('util');
 var fs = require('fs');
-var orderutil=require("../util/index.js");
+var orderutil = require("../util/index.js");
 module.exports = function (app) {
     /**
      * 保存用户订单信息
      */
     app.post('/ordersave', function (req, res, next) {
-        const orderuid=orderutil.orederNumber(req.body.shopnumber);
+        const orderuid = orderutil.orederNumber(req.body.shopnumber);
         order.savaoreder({
             data: {
                 shopname: req.body.shopname,
@@ -20,32 +20,50 @@ module.exports = function (app) {
                 shopnum: req.body.shopnum,
                 username: '某某某',
                 orderuid: orderuid,
-                status:0,
-                shopid:req.body.id,
+                status: 0,
+                shopid: req.body.id,
                 ordertime: moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
             },
             sql: "INSERT INTO `order` SET ?"
         }, function (data) {
             if (data.status) {
-                order.updateShop({
-                    sql: "update shop SET stocknum=?,outstocktime=? WHERE id = ?",
-                    params: [
-                        req.body.stocknum,
-                        moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
-                        req.body.id
-                    ]
-                }, function (shopdata) {
-                    if (shopdata.status) {
-                        shop.updateStock({
-                            sql: `UPDATE stock SET total = (SELECT SUM(stocknum) FROM shop), 
+                //判断如果商品库存为0的时候删除该商品
+                if (req.body.stocknum == 0) {
+                    shop.deleteShops({
+                        sql: "DELETE FROM shop WHERE id = " + req.body.id
+                    }, function (shopdata) {
+                        if (shopdata.status) {
+                            shop.updateStock({
+                                sql: `UPDATE stock SET total = (SELECT SUM(stocknum) FROM shop), 
                                 typecostume = (SELECT SUM(stocknum) FROM shop s WHERE s.type = 0),
                                 typeelectrical = (SELECT SUM(stocknum) FROM shop s WHERE s.type = 1),
                                 typedigital = (SELECT SUM(stocknum) FROM shop s WHERE s.type = 2)`
-                        }, function (data) {
-                        })
-                        res.send(data);
-                    }
-                });
+                            }, function (data) {
+                            })
+                            res.send(data);
+                        }
+                    });
+                } else {
+                    order.updateShop({
+                        sql: "update shop SET stocknum=?,outstocktime=? WHERE id = ?",
+                        params: [
+                            req.body.stocknum,
+                            moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
+                            req.body.id
+                        ]
+                    }, function (shopdata) {
+                        if (shopdata.status) {
+                            shop.updateStock({
+                                sql: `UPDATE stock SET total = (SELECT SUM(stocknum) FROM shop), 
+                                typecostume = (SELECT SUM(stocknum) FROM shop s WHERE s.type = 0),
+                                typeelectrical = (SELECT SUM(stocknum) FROM shop s WHERE s.type = 1),
+                                typedigital = (SELECT SUM(stocknum) FROM shop s WHERE s.type = 2)`
+                            }, function (data) {
+                            })
+                            res.send(data);
+                        }
+                    });
+                }
             }
         });
     });
